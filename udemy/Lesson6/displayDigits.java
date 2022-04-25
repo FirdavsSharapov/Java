4e7c6d62b43282109a8327d07bfec15079bba1eb
Fred Sharapov <f.sharapov@yahoo.com>
2022-04-25 08:41:33 -0400

    conditional statements

---
+++ udemy/Lesson6/displayDigits.java
@@ -0,0 +1,21 @@
+package udemy.Lesson6;
+
+import java.util.Scanner;
+
+public class displayDigits {
+    public static void main(String[] args) {
+
+        System.out.println("Enter a number");
+        Scanner digit=new Scanner(System.in);
+        int number =digit.nextInt();
+        int reminder=0;
+        while(number>0){
+            reminder=number%10;
+            System.out.println(reminder);
+            number=number/10;
+            System.out.println(number);
+        }
+
+    }
+    
+}
---
+++ udemy/Lesson6/displayMultiplicationTable.java
@@ -0,0 +1,15 @@
+package udemy.Lesson6;
+
+import java.util.Scanner;
+
+public class displayMultiplicationTable {
+    public static void main(String[] args) {
+        System.out.print("Print number that you want to have multiplication table: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " * " + number + " = " + number * i);
+        }
+
+    }
+}
