0f046593beaeba2e169903b9b2ebf6907f45867f
Fred Sharapov <f.sharapov@yahoo.com>
2022-08-22 09:11:57 -0400

    old commits

---
+++ udemy/Lesson13/Finalpractice.java
@@ -0,0 +1,9 @@
+package udemy.Lesson13;
+
+public class Finalpractice {
+    public static void main(String[] args) {
+        final float PI = 3.1425f;
+        System.out.println(PI);
+
+    }
+}
---
+++ udemy/Lesson13/StaticMethodPractice.java
@@ -0,0 +1,28 @@
+package udemy.Lesson13;
+
+class Test
+{
+    static int x = 20;
+    int y =30;
+
+    void show(){
+        System.out.println(x + " " + y);
+    }
+
+    static void display()
+    {
+        System.out.println(x);
+    }
+}
+
+public class StaticMethodPractice{
+    public static void main(String[] args) {
+        Test t1 = new Test();
+        t1.show();
+        t1.x = 40;
+        t1.y = 60;
+
+        Test t2 = new Test();
+        t2.show();
+    }
+}
